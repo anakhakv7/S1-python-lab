8fa66438b06eeda7cf3a1934458ad26b5d5cb83f
anakhakv7 <anqakv7@gmail.com>
2025-09-26 11:42:57 +0530

    Delete display1st&lastclrs.py

--- display1st&lastclrs.py
+++
@@ -1,7 +0,0 @@
-colors_input = input("Enter colors separated by commas: ")
-colors_list = [color.strip() for color in colors_input.split(',')]
-if colors_list:
-    print("First color:", colors_list[0])
-    print("Last color:", colors_list[-1])
-else:
-    print("No colors entered.")
