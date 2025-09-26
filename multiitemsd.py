7e69069ed30634b52f51d18d4e23026d4f545cea
anakhakv7 <anqakv7@gmail.com>
2025-09-26 11:40:57 +0530

    Add files via upload

---
+++ multiitemsd.py
@@ -0,0 +1,5 @@
+mydict={"data1":11,"data2":4,"data3":2}
+total=1
+for i in mydict:
+    total=total*mydict[i]
+print("The multiplication of all values in the dictionary is:",total)
---
+++ over.py
@@ -0,0 +1,9 @@
+number=input("Enter 5 number:" ,s.split())
+for num in number:
+    if num>100:
+        print("over")
+    else:
+        print(number)
+
+
+
